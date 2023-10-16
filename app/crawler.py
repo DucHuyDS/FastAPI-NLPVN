f267147c0b24fba26a27fb7e2253c9d4aa895997
DucHuyDS <96015433+DucHuyDS@users.noreply.github.com>
2023-10-16 21:52:05 +0700

    Update crawler.py

--- app/crawler.py
+++ app/crawler.py
@@ -1,53 +1,36 @@
-import re
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
-
+import pathlib
+from utils import clean_html
+from webdriver_manager.chrome import ChromeDriverManager
+import asyncio
 load_dotenv()
+base_dir = pathlib.Path(__file__).parent.parent.resolve()
 
 
-def preprocessing(sentence):
-    text = re.sub(r'\s+', ' ', str(sentence))  # remove white space
-    text = re.sub(r'<a.+?</a> ', '', text)
-    text = re.sub('<.*?>', '', text)  # remove tag
-    text = re.sub(r'http\S+', '', text)  # remove the link
-    text = re.sub('&gt;', '>', text)
-    text = re.sub('&lt;', '<', text)
-    return text
 
-
-async def parse_comment(link, websocket):
+async def get_comments(link, websocket):
     options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-ssl-errors=yes')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument("--incognito")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--window-size=1920,1080") 
     options.add_argument("--disable-extensions")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--disable-infobars')
+    options.add_argument('--remote-debugging-port=9222')
 
-    driver = webdriver.Remote(
-    command_executor='http://selenium:4444',
-    options=options
-    )
-    # driver = webdriver.Remote(
-    # command_executor='http://localhost:4444',
-    # options=options
-    # )
- 
+    driver= webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver.get('https://www.facebook.com/')
+    driver.execute_script("javascript: void(function () {function setCookie(t) {var list = t.split('; ');console.log(list);for (var i = list.length - 1; i >= 0; i--) {var cname = list[i].split('=')[0];var cvalue = list[i].split('=')[1];var d = new Date();d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));var expires = ';domain=.facebook.com;expires=' + d.toUTCString();document.cookie = cname + '=' + cvalue + '; ' + expires;}}function hex2a(hex) {var str = '';for (var i = 0; i < hex.length; i += 2) {var v = parseInt(hex.substr(i, 2), 16);if (v) str += String.fromCharCode(v);}return str;}setCookie('"+os.getenv('cookie')+"');location.href = 'https://facebook.com';})();")
 
-    if 'watch' in link:
-
-        driver.get('https://www.facebook.com/')
-        driver.find_element(By.ID, 'email').send_keys(os.getenv('email'))
-        driver.find_element(By.ID, 'pass').send_keys(os.getenv('password'))
-        driver.find_element(By.NAME, 'login').click()
-        time.sleep(10)
+    time.sleep(5)
+    if 'watch' in link or 'videos' in link:
         driver.get(link)
         current_url = driver.current_url
         new_link = current_url.replace('www.', 'mbasic.')
@@ -60,12 +43,14 @@ async def parse_comment(link, websocket):
     list_comments = []
     for i in range(0, 41, 10):
         try:
-            get_div_by_id = driver.find_elements(By.XPATH, "//div[starts-with(@id, 'ufi_')]")
-            get_html = get_div_by_id[0].get_attribute('innerHTML')
+            
+            get_div_by_id = driver.find_elements(By.XPATH, "//div[starts-with(@id, 'ufi_')]")#find the first post
+            get_html = get_div_by_id[0].get_attribute('innerHTML')#get html
 
             html = BeautifulSoup(str(get_html), "html.parser")
-            classes_of_elements = html.find_all('div')
-            # print(classes_of_elements)
+            classes_of_elements = html.find_all('div')# get div
+            if len(classes_of_elements) <= 10: #the end of comment
+                break
             # get class comment
             for i in range(len(classes_of_elements)):
                 if classes_of_elements[i].get('class') == None:
@@ -73,22 +58,18 @@ async def parse_comment(link, websocket):
                     break
         except IndexError:
             raise IndexError("error")
-         
-            # print('error: ' + str(e))
-            # break
 
-        comments = html.find_all("div", class_=get_class)
-        # href = html.find_all('a')
-        # try: # comment not found
-        #     href = href[-1].get('href')
-        # except IndexError:
-        #     break
+
+        comments = html.find_all("div", class_=get_class)#find all divs with the same class
+
         for comment in comments:
-            content = preprocessing(comment)
-            await websocket.send_json({'content': content})
+            content = clean_html(comment)
+            # print(content)
+            await websocket.send_json({'content': f"->{content}"})
+            await asyncio.sleep(0.05)
             list_comments.append(content)
         try:
-            driver.find_elements(By.XPATH, "//div[starts-with(@id, 'see_next_')]/a")[0].click()
+            driver.find_elements(By.XPATH, "//div[starts-with(@id, 'see_next_')]/a")[0].click() #next page and get comments
         except IndexError:
             break
     driver.close()
