5644b04ade8c85b003fee168e92116cf3dbf785a
DucHuyDS <96015433+DucHuyDS@users.noreply.github.com>
2023-10-17 15:44:54 +0700

    Update crawler.py

--- app/crawler.py
+++ app/crawler.py
@@ -8,6 +8,9 @@ import pathlib
 from utils import clean_html
 from webdriver_manager.chrome import ChromeDriverManager
 import asyncio
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 load_dotenv()
 base_dir = pathlib.Path(__file__).parent.parent.resolve()
 
@@ -29,17 +32,18 @@ async def get_comments(link, websocket):
     driver.get('https://www.facebook.com/')
     driver.execute_script("javascript: void(function () {function setCookie(t) {var list = t.split('; ');console.log(list);for (var i = list.length - 1; i >= 0; i--) {var cname = list[i].split('=')[0];var cvalue = list[i].split('=')[1];var d = new Date();d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));var expires = ';domain=.facebook.com;expires=' + d.toUTCString();document.cookie = cname + '=' + cvalue + '; ' + expires;}}function hex2a(hex) {var str = '';for (var i = 0; i < hex.length; i += 2) {var v = parseInt(hex.substr(i, 2), 16);if (v) str += String.fromCharCode(v);}return str;}setCookie('"+os.getenv('cookie')+"');location.href = 'https://facebook.com';})();")
 
-    time.sleep(5)
-    if 'watch' in link or 'videos' in link:
-        driver.get(link)
-        current_url = driver.current_url
-        new_link = current_url.replace('www.', 'mbasic.')
-        driver.get(new_link)
-        driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/table/tbody/tr/td/div[2]/div/article/footer/div[2]/a[1]').click()
-    else:
-        new_link = link.replace('www.', 'mbasic.')
-        driver.get(new_link)
+    # time.sleep(5)
+    delay = 5 # seconds
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'facebook')))
 
+
+    new_link = link.replace('www.', 'mbasic.')
+    driver.get(new_link)
+    current_url = driver.current_url
+    # print(current_url)
+    if 'watch' in current_url or 'videos' in current_url:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'meta_referrer')))
+        driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/table/tbody/tr/td/div[2]/div/article/footer/div[2]/a[1]').click()
     list_comments = []
     for i in range(0, 41, 10):
         try:
